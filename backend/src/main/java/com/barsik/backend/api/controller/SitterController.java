da5acad0d7c73f2df266200fb5dd79b3475ee71b
Aglaya <aglajaalin@gmail.com>
2026-02-25 10:43:58 +0300

    fix buil error

--- backend/src/main/java/com/barsik/backend/api/controller/SitterController.java
+++ backend/src/main/java/com/barsik/backend/api/controller/SitterController.java
@@ -1,10 +1,10 @@
 package com.barsik.backend.api.controller;
 
 import java.math.BigDecimal;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
--- backend/src/main/java/com/barsik/backend/security/CustomHandshakeHandler.java
+++ backend/src/main/java/com/barsik/backend/security/CustomHandshakeHandler.java
@@ -5,9 +5,11 @@ import java.util.Map;
 
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.stereotype.Component;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
 
+@Component
 public class CustomHandshakeHandler extends DefaultHandshakeHandler {
 
     @Override
